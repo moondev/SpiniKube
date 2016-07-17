7267f13888297b8ddcc00f2bde3e59af48a65c10
Chad Moon <chad.d.moon@gmail.com>
2016-07-17 13:18:24 -0600

    restart script and more conf

--- restart.py
+++ restart.py
@@ -17,51 +17,31 @@ def poll():
     time.sleep(2)
 
 
-os.system("minikube delete")
-os.system("minikube start --memory 8000 --cpus 2")
-time.sleep(10)
-poll()
-os.system("kubectl create namespace spinnaker")
 
-os.system("rm minikube/apiserver.crt")
-os.system("rm minikube/apiserver.key")
-os.system("rm minikube/ca.crt")
-
-# ip = os.popen('minikube ip').read().strip()
-
-# os.system("cp ~/.kube/config minikube/config2")
-
-# with open("minikube/config", "wt") as fout:
-#   with open("/home/chad/.kube/config", "rt") as fin:
-#     for line in fin:
-#       fout.write(line.replace('/home/chad', '/root'))
-#       fout.write(line.replace('.minikube', '.kube'))
-
-
-os.system("cp ~/.minikube/apiserver.crt minikube/apiserver.crt")
-os.system("cp ~/.minikube/apiserver.key minikube/apiserver.key")
-os.system("cp ~/.minikube/ca.crt minikube/ca.crt")
-
-time.sleep(1)
+os.system("kubectl delete secret spinnaker-config --namespace=spinnaker")
+os.system("kubectl delete secret minikube-config --namespace=spinnaker")
+os.system("kubectl delete secret nginx-config --namespace=spinnaker")
 
 os.system("kubectl create secret generic spinnaker-config --from-file=./config/gate.yml --from-file=./config/orca.yml --from-file=./config/rosco.yml --from-file=./config/front50.yml --from-file=./config/clouddriver.yml --namespace spinnaker")
-
-
-
 
 
 os.system("kubectl create secret generic minikube-config --from-file=./minikube/config --from-file=./minikube/ca.crt --from-file=./minikube/apiserver.crt --from-file=./minikube/apiserver.key --namespace spinnaker")
 
 os.system("kubectl create secret generic nginx-config --from-file=./config/nginx.conf --namespace spinnaker")
 
+os.system("kubectl delete deployment spin-front50 --namespace=spinnaker")
+os.system("kubectl delete deployment spin-clouddriver --namespace=spinnaker")
+os.system("kubectl delete deployment spin-rosco --namespace=spinnaker")
+os.system("kubectl delete deployment spin-orca --namespace=spinnaker")
+os.system("kubectl delete deployment spin-gate --namespace=spinnaker")
 
-os.system("kubectl create --namespace spinnaker -f sets/cassandra.yml")
-time.sleep(1)
-os.system("kubectl create --namespace spinnaker -f services/cassandra.json")
+os.system("kubectl delete service spin-front50 --namespace=spinnaker")
+os.system("kubectl delete service spin-clouddriver --namespace=spinnaker")
+os.system("kubectl delete service spin-rosco --namespace=spinnaker")
+os.system("kubectl delete service spin-orca --namespace=spinnaker")
+os.system("kubectl delete service spin-gate --namespace=spinnaker")
 
-os.system("kubectl create --namespace spinnaker -f sets/redis.yml")
-time.sleep(1)
-os.system("kubectl create --namespace spinnaker -f services/redis.json")
+
 
 os.system("kubectl create --namespace spinnaker -f sets/front50.yml")
 time.sleep(1)
@@ -83,16 +63,11 @@ os.system("kubectl create --namespace spinnaker -f sets/gate.yml")
 time.sleep(1)
 os.system("kubectl create --namespace spinnaker -f services/gate.json")
 
-#os.system("minikube ssh 'sudo mount -t vboxsf hosthome /hosthome''")
-
-os.system("kubectl create --namespace spinnaker -f sets/deck.yml")
-time.sleep(1)
-os.system('kubectl expose deployment spin-deck --namespace spinnaker --type=NodePort')
 
 
 poll()
 
-os.system("minikube dashboard")
-os.system('minikube service spin-deck --namespace spinnaker')
+#os.system("minikube dashboard")
+
 
 #mount -t vboxsf hosthome /hosthome
