e219f4d24443c652622ff4552a09c1ca1805a02c
Chad Moon <cmoon@kenzan.com>
2016-07-29 14:44:00 -0600

    example app and other sets

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def poll():
 
 os.system("minikube delete")
 time.sleep(5)
-os.system("minikube start --memory 10000 --cpus 2 --disk-size=40g")
+os.system("minikube start --memory 10000 --cpus 2 --disk-size=60g")
 time.sleep(10)
 poll()
 
